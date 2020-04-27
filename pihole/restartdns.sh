3f4100a8d682995361b10a91137a011ac605da52
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2020-04-27 22:12:29 +0100

    bug: output date

--- pihole/restartdns.sh
+++ pihole/restartdns.sh
@@ -5,6 +5,6 @@
 WEBSITEURL="https://denhamparry.co.uk"
 
 if (( $( curl $WEBSITEURL | grep -c "incident-title" ) > 0 )) ; 
-then pihole restartdns && echo "${DATE} - restart dns" >> /tmp/dns.output;
-else "${DATE} - dns ok" >> /tmp/dnscheck.output;
+then pihole restartdns && date >> /tmp/dns.output;
+else date > /tmp/dnscheck.output;
 fi
