8bc774a472310d6ecb3135b22ba94bb5750d226b
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2020-04-27 22:01:00 +0100

    feat: tidy restartdns

--- pihole/restartdns.sh
+++ pihole/restartdns.sh
@@ -5,6 +5,5 @@
 WEBSITEURL="https://denhamparry.co.uk"
 
 if (( $( curl $WEBSITEURL | grep -c "incident-title" ) > 0 )) ; 
-then pihole restartdns && echo "Restarted DNS" > /tmp/dns.output;
-else pihole restartdns && echo "Fully Operational" > /tmp/dns.output;
+then pihole restartdns;
 fi
